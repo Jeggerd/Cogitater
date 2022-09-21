84a36f023bb3e076c7eb89521385f1b32c902db0
Jeggerd <112284773+Jeggerd@users.noreply.github.com>
2022-09-21 09:59:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function copy(id) {
     var r = document.createRange();
     var e = document.querySelector('.tooltiptext')
@@ -9,9 +7,6 @@ function copy(id) {
     window.getSelection().addRange(r);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
-
-    // e.textContent = "Copied!"
-    // e.style.opacity = 1;
 
     var copy = document.querySelector('#copy-success')
     var contact = document.querySelector('#contact-us')
@@ -23,53 +18,53 @@ function copy(id) {
         copy.style.opacity = 0
         contact.style.opacity = 1
     }, 2000);
-
-    // document.querySelector('#credit')
-    // setTimeout(function(){e.style.opacity = 0; setTimeout(function(){e.textContent = ""}, 250)}, 1250);
 }
 
+var didmakestars = false
 window.onload = function generateStars(){
-    for (let i = 180; i--;) {
-        let div = document.createElement("div");
-        let aura = document.createElement("div");
+    if (!didmakestars) {
+        for (let i = 180; i--;) {
+            let div = document.createElement("div");
+            let aura = document.createElement("div");
 
-        let bool = Math.random() < 0.25;
-        if (bool == true) {
-            let bool = Math.random() < 0.5;
+            let bool = Math.random() < 0.25;
             if (bool == true) {
-                div.style.backgroundColor = "var(--star-color-p)"
-                aura.style.backgroundColor = "var(--star-color-p) !important"
-            } else {
-                div.style.backgroundColor = "var(--star-color-b)"
-                aura.style.backgroundColor = "var(--star-color-b) !important"
+                let bool = Math.random() < 0.5;
+                if (bool == true) {
+                    div.style.backgroundColor = "var(--star-color-p)"
+                    aura.style.backgroundColor = "var(--star-color-p) !important"
+                } else {
+                    div.style.backgroundColor = "var(--star-color-b)"
+                    aura.style.backgroundColor = "var(--star-color-b) !important"
+                }
             }
+
+            var size = Math.random() * (2) + 2;
+            size = Math.round(size)
+            let w = document.documentElement.clientWidth
+            let h = document.documentElement.clientHeight
+
+            let posx = (Math.random() * (w-4));
+            let posy = (Math.random() * (h-4));
+
+            div.className    = "star";
+            div.style.left   = (posx / w)*100 + '%'
+            div.style.top    = (posy / h)*100 + '%'
+            div.style.width  = size + "px"
+            div.style.height = size + "px"
+
+            document.querySelector("#back").appendChild(div)
+
+            aura.classList     = "star aura";
+            aura.style.left    = (posx / w)*100 + '%'
+            aura.style.top     = (posy / h)*100 + '%'
+            aura.style.width   = size + 18 + "px"
+            aura.style.height  = size + 18 + "px"
+            aura.style.margin  = -10 + "px"
+
+            document.querySelector("#back").appendChild(aura)
         }
 
-        var size = Math.random() * (2) + 2;
-        size = Math.round(size)
-        let w = document.documentElement.clientWidth
-        let h = document.documentElement.clientHeight
-
-        let posx = (Math.random() * (w-4));
-        let posy = (Math.random() * (h-4));
-
-        div.className    = "star";
-        div.style.left   = (posx / w)*100 + '%'
-        div.style.top    = (posy / h)*100 + '%'
-        div.style.width  = size + "px"
-        div.style.height = size + "px"
-
-        document.querySelector("#back").appendChild(div)
-
-        aura.classList     = "star aura";
-        aura.style.left    = (posx / w)*100 + '%'
-        aura.style.top     = (posy / h)*100 + '%'
-        aura.style.width   = size + 18 + "px"
-        aura.style.height  = size + 18 + "px"
-        aura.style.margin  = -10 + "px"
-
-        document.querySelector("#back").appendChild(aura)
+        didmakestars = true
     }
 }
-
-  
